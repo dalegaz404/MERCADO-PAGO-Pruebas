9bfbbfc8fd1f52d9aed826b8db807f34611ef587
dalegaz404 <108595772+dalegaz404@users.noreply.github.com>
2023-06-30 00:13:54 -0300

    Delete MERCADO-PAGO-Pruebas/BACK directory

--- MERCADO-PAGO-Pruebas/BACK/src/models/Fotoprod.js
+++
@@ -1,8 +0,0 @@
-const { DataTypes } = require('sequelize');
-module.exports = (sequelize) => {
- sequelize.define('fotoprod', {
-  id: {  type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  urlfotoprod: { type: DataTypes.STRING, allowNull: true, },
-  idproducto: { type: DataTypes.INTEGER, allowNull: true, },
-  });
-};
--- MERCADO-PAGO-Pruebas/BACK/src/models/categoria.js
+++
@@ -1,8 +0,0 @@
-const { DataTypes } = require('sequelize');
-module.exports = (sequelize) => {
- sequelize.define('categoria', {
-  id: {  type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  nombrecat: { type: DataTypes.STRING, allowNull: true, },
-  desccat: { type: DataTypes.STRING, allowNull: true, },
-  });
-};
